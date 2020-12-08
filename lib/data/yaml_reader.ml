ebdc638689e3f0cfa42176569713cd9d4cf531a1
Ry Wiese <rywiese@mn-rwiese-m.ad.smartthings.com>
2020-12-08 01:25:19 -0600

    Works

--- lib/data/yaml_reader.ml
+++ lib/data/yaml_reader.ml
@@ -5,15 +5,6 @@ exception Configuration_exception of string
 let get_speaker_by_id (id : string) : Speaker_yml_entity.t =
 	{ id = id; username = "ry"; host = "linuxbook" }
 
-let list_speakers_old () : Speaker_yml_entity.t list =
-	[
-		get_speaker_by_id "speaker 1";
-		get_speaker_by_id "speaker 2";
-		get_speaker_by_id "speaker 3";
-	]
-
-let speaker1 = ("Desk Speakers", `O [("username", `String "ry"); ("host", `String "linuxbook")])
-
 let speaker_of_yml (yml : string * Yaml.value) : Speaker_yml_entity.t =
 	let (id, body) = yml in
 	match body with
