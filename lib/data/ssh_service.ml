1222d5a7769696854844b04b218af2e6c77a538b
Ry Wiese <rywiese@mn-rwiese-m.ad.smartthings.com>
2020-12-12 21:34:22 -0600

    Adds ping before ssh. Sucks that we have to do this. But, everything works! Can enable and disable speakers via postman

--- lib/data/ssh_service.ml
+++ lib/data/ssh_service.ml
@@ -3,11 +3,13 @@ open Model
 let is_speaker_enabled (_speaker : Speaker_yml_entity.t) : bool = true
 
 let enable_speaker (speaker : Speaker_yml_entity.t) : unit =
-	Printf.sprintf "ssh %s@%s sudo systemctl start snapclient.service" speaker.username speaker.host
+	(* Remove the ping when we get the linuxbook network thing figured out... *)
+	Printf.sprintf "ping -t 1 %s; ssh %s@%s sudo systemctl start snapclient.service" speaker.host speaker.username speaker.host
 	|> Sys.command
 	|> ignore
 
 let disable_speaker (speaker : Speaker_yml_entity.t) : unit =
-	Printf.sprintf "ssh %s@%s sudo systemctl stop snapclient.service" speaker.username speaker.host
+	(* Remove the ping when we get the linuxbook network thing figured out... *)
+	Printf.sprintf "ping -t 1 %s; ssh %s@%s sudo systemctl stop snapclient.service" speaker.host speaker.username speaker.host
 	|> Sys.command
 	|> ignore
