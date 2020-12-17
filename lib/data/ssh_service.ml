3d597a498707010d0f725c363582fcda1d669f7d
Ry Wiese <rywiese@mn-rwiese-m.ad.smartthings.com>
2020-12-16 20:51:24 -0600

    Return is_enabled

--- lib/data/ssh_service.ml
+++ lib/data/ssh_service.ml
@@ -1,6 +1,20 @@
 open Model
 
-let is_speaker_enabled (_speaker : Speaker_yml_entity.t) : bool = true
+let command = "sudo systemctl status snapclient.service | grep \"Active: active (running)\""
+
+
+let is_speaker_enabled (speaker : Speaker_yml_entity.t) : bool =
+	let session = Ssh.init () in
+	let opts = Ssh.Client.({
+		host = speaker.host;
+		log_level = SSH_LOG_NOLOG;
+		port = 22;
+		username = speaker.username;
+		auth = Auto;
+	}) in
+	Ssh.Client.connect opts session;
+	session |> Ssh.Client.exec ~command:command |> String.length
+	|> (fun s -> s <> 0)
 
 let enable_speaker (speaker : Speaker_yml_entity.t) : unit =
 	(* Remove the ping when we get the linuxbook network thing figured out... *)
